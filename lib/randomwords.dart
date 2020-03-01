b2e5d59734eba0cf08562fab5a2f4a9ed3013d7c
Christofer Chavez <xnpiochv@gmail.com>
2020-03-01 13:52:20 -0500

    Part 1 finished

---
+++ lib/randomwords.dart
@@ -0,0 +1,54 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RandomWords extends StatefulWidget{
+
+  @override
+  State<StatefulWidget> createState(){
+    return RandomWordsState();
+  }
+
+}
+
+class RandomWordsState extends State<RandomWords>{
+
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  Widget _buildSuggestions(){
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (context, i){
+          if(i.isOdd){
+            return Divider();
+          }
+          final int index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+}
